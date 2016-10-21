40ec017e3e240e13cc54fab8584d188cd783cd04
perlancar (@pc-office) <perlancar@gmail.com>
2016-10-21 20:37:55 +0700

    Initial commit

---
+++ lib/App/GetoptLongSubcommandUtils.pm
@@ -0,0 +1,21 @@
+package App::GetoptLongSubcommandUtils;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Collection of CLI utilities related to Getopt::Long::Subcommand
+
+=head1 DESCRIPTION
+
+This distribution provides the following command-line utilities related to
+L<Getopt::Long::Subcommand>:
+
+#INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<Getopt::Long::Subcommand>
+
+=cut
